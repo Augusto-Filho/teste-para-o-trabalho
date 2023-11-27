eace3ad47012b894a66a3c50ce6b2e4885418a34
augus <augus@LAPTOP-7U7LSCKT>
2023-11-27 20:04:19 -0300

    Teste Commit para personagem de jogo

---
+++ Testejogo/src/TesteGit.java
@@ -0,0 +1,33 @@
+
+public class TesteGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class PersonagemAugusto {
+		    String classe;
+		    int level;
+		    String habilidadePrincipal;
+
+		        public PersonagemAugusto() {
+		        }
+
+		        public void mostrarStatusAugusto() {
+		            System.out.format("Classe: %s\nLevel: %d\nHabilidade Principal: %s\n", classe, level, habilidadePrincipal);
+		        }
+
+		       public void buffBerserker(String aliadoAugusto) {
+		            System.out.println("Concede 15% de dano extra!");
+		        }
+
+		        void ataqueBerserker(String aliadoAugusto) {
+		            System.out.println("Augusto ativa sua habilidade principal, atacando freneticamente o inimigo com seu machado!");
+		        }
+
+		    public void buffArqueiroElfico(String aliadoPedro) {
+		        System.out.println("Pedro concede 25% de aumento de precisão de ataque!");
+
+		    }
+		}
+	}
+
+}
